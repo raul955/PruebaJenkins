beaa12234e3c057de6c54ad3792d535332d28398
raul955 <43537133+raul955@users.noreply.github.com>
2019-11-20 08:56:12 +0100

    Add files via upload
    Hola mundo en spring boot para testear jenkins+sonarqube

---
+++ src/main/java/ejemplo/SpringBootApplicationLauncher.java
@@ -0,0 +1,9 @@
+package ejemplo;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringBootApplicationLauncher {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldController.class, args);
+	}
+}
